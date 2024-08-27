b97fe9267f737d04f485df8ace9e9074b93606b6
Iwmvictor <isingizwemunezerovictor5@gmail.com>
2024-08-27 16:54:08 +0200

    Initial commit

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 
 function Blog() {
   return (
@@ -12,7 +13,7 @@ function Blog() {
           />
           <meta
             name="keywords"
-            content="Blog, IT insights, software development trends, design updates, Kampus Agency, global team"
+            content="Blog, IT insights, software development trends, design updates, Kampus Agency, global team, Graphic Design Trends, Web Development Tutorials, Branding Tips, UI/UX Best Practices, Software Development Trends, SEO Strategies, Digital Marketing Tips, App Development Insights, Ecommerce Design Ideas, Content Marketing, Marketing Automation, Web Design Tips, Corporate Branding, Online Business Growth, Digital Branding, Marketing for Startups, Conversion Optimization, Social Media Strategies, SEO for Businesses, Mobile App Design, Print Design Best Practices, Marketing Campaign Ideas, Video Marketing Tips, Custom Merchandise Design, IT Security Tips, Digital Transformation, Tech for Small Businesses, Latest Design Trends in 2024, How to Optimize Website Speed, Importance of Branding for Startups, Essential UI/UX Principles, Developing Scalable Web Applications, How to Build Effective Marketing Funnels, Strategies for Successful Video Marketing, How to Create Engaging Social Media Campaigns, How to Choose the Right Ecommerce Platform, Crafting the Perfect Brand Story, Best Practices for Web Accessibility, Importance of Mobile-First Web Design, Using Animations for User Engagement, The Future of Ecommerce, Creating Viral Marketing Campaigns, Optimizing Websites for Local SEO, Leveraging Data Analytics for Business Growth, How to Use Infographics in Content Marketing, Designing for Print vs Digital, Integrating Motion Graphics in Marketing, The Ultimate Guide to Modern Web Design in 2024, 10 Essential Tips for Building an Engaging Brand Identity, How to Create a User-Centered Website that Converts, 5 Ways to Improve Mobile App Usability, Top Digital Marketing Strategies to Skyrocket Your Business, Best Practices for Secure Software Development, How to Create Compelling Product Demo Videos, The Role of UI/UX in Building a Successful Ecommerce Store, Why Branding is Key to, viral company, singapole of africa, rwanda, heart of africa, ramboghini"
           />
           <meta property="og:title" content="Blog | Kampus Agency" />
           <meta
