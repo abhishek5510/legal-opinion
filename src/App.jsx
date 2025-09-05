c71c7114ef67842dda5f28a43be85e24032ddc13
priti1234k <pritikkumari3@gmail.com>
2025-09-05 18:45:39 +0530

    Added Calender Intergration

--- src/App.jsx
+++ src/App.jsx
@@ -28,13 +28,8 @@ function App() {
             <Route index element={<Home />} />
             <Route path="*" element={<NotFound />} />
             <Route path="about" element={<About />} />
-            <Route path="articles" element={<Blog />} />
-            <Route path="get-start" element={<Start />} />
-            <Route path="courses" element={<Courses />} />
             <Route path="contacts" element={<Contact />} />
             <Route path="services" element={<Services />} />
-            <Route path="projects" element={<Portfolio />} />
-            <Route path="resources" element={<Resources />} />
             <Route path="testimonials" element={<Testimonials />} />
           </Route>
         </Routes>
--- src/servicepage/Hero.jsx
+++ src/servicepage/Hero.jsx
@@ -13,8 +13,19 @@ function Hero() {
               From verification to full legal coverage, we safeguard your land.
               Think of it as insurance for your peace of mind.
               Your property, protected today and tomorrow.
+              <hr />
+              <div style={{ fontSize: '16px', fontWeight: '400', marginTop: '10px' }}>
+                Click below to schedule a call with our experts.
+              </div>
             </p>
-            <a href="/projects">Explore what we've built</a>
+            <a
+              href="https://cal.com/theopinionbench/30min?overlayCalendar=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Get In Touch
+            </a>
+
           </div>
         </div>
       </div>
